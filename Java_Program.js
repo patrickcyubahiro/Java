e0efe8c30dba5edacd15c994b425748f55abdc6a
Patrick CYUBAHIRO <ampatrickcyubahiro@gmail.com>
2019-12-06 15:48:10 +0300

    Create Java_Program.js

---
+++ Java_Program.js
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bbitnov1b;
+
+/**
+ *
+ * @author Patrick CYUBAHIRO
+ */
+public class BBITNOV1B {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String First_name = "Patrick";
+        String Last_name = "CYUBAHIRO";
+        int age = 22;
+        
+        
+        // TODO code application logic here
+        System.out.print("Welcome to Java Programming\n");
+        System.out.println("My name is "+First_name);
+        System.out.println("My age is "+age);
+    }
+    
+}
